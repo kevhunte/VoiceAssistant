d5227b2476ee8f5d18e18f1abc92f52afb6f7142
kevhunte <kevinmhunte@gmail.com>
2020-12-30 10:11:02 -0500

    Basic skeleton

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 #/usr/bin/python3
 import speech_recognition as sr
+from time import ctime
 
 r = sr.Recognizer()
+NAME = "Ant"
 
-with sr.Microphone() as source:
-    print('running...')
-    audio = r.listen(source)
-    voice_data = r.recognize_google(audio)
-    print(voice_data)
+
+'''
+speech to text
+'''
+def record_command():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry I did not understand that command')
+        except sr.RequestError:
+            print('Sorry my service is unavailable')
+        return voice_data
+
+'''
+business logic
+'''
+def process_command(voice_data):
+    if 'what is your name' in voice_data:
+        print(f"My name is {NAME}")
+    if 'what time is it' in voice_data:
+        print(f"The current time is {ctime()}")
+    if 'stop' in voice_data or 'shut down' in voice_data:
+        print('Goodbye')
+        exit()
+
+
+def handler():
+    print("How can I assist you?")
+    while True:
+        voice_data = record_command()
+        print(f"you said: {voice_data}")
+        process_command(voice_data)
+
+if __name__ == '__main__':
+    handler()
